18e1252e3064bd2d7914614df1f8a8f0f80ecf32
2022project <ghkdsigm3@gmail.com>
2022-03-08 02:35:08 +0900

    api 호출

---
+++ src/components/Login.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <input type="text">
+    <button type="button" @click="loginUser">로그인</button>
+    <ul>
+      <li v-for="item in users" :key="item" >
+        {{item.name}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data(){
+    return{
+      users:[],
+    }
+  },
+  methods:{
+    loginUser(){
+      axios.get('https://jsonplaceholder.typicode.com/users/1')
+      .then(res =>{
+        if(res.data.id === 1){
+          console.log('사용자가 인증됨')
+          this.users = res.data
+        }
+      })
+      .catch(err=>{
+        console.log(err)
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
